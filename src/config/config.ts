5ff86e5be3237b9ce51693b028883af67f5976ba
Horacio Martinez <litstyle17@gmail.com>
2023-09-07 19:35:39 +0200

    feat: Add .env.example file and utilities for Cloudinary management
    Added an .env.example file with sample data for the environment variables used in the application. Additionally, a new utilities file has been included in the utils folder to manage image uploads to Cloudinary. Teammates can use the .env.example file for reference and Cloudinary utilities to efficiently upload images.

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ type EnvironmentConfig = {
     app: AppConfig;
     db: MongoDBConfig;
     auth: AuthConfig
+    cloudinary: cloudinaryConfig;
 };
 
 type AppConfig = { PORT: string | number };
@@ -16,6 +17,12 @@ type AuthConfig = {
     client_origin: string | undefined;
     audience: string | undefined;
     issuer: string | undefined;
+}
+
+type cloudinaryConfig = {
+    cloud_name: string | undefined;
+    api_key: string | undefined;
+    api_secret: string | undefined;
 }
 
 if (process.env.NODE_ENV === 'production') dotenv.config({ path: '.env.production' });
@@ -36,6 +43,11 @@ const CONFIG: TConfig = {
             client_origin: process.env.APP_ORIGIN,
             audience: process.env.AUTH0_AUDIENCE,
             issuer: process.env.AUTH0_ISSUER
+        },
+        cloudinary: {
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
         }
     },
     production: {
@@ -49,6 +61,11 @@ const CONFIG: TConfig = {
             client_origin: process.env.APP_ORIGIN,
             audience: process.env.AUTH0_AUDIENCE,
             issuer: process.env.AUTH0_ISSUER
+        },
+        cloudinary: {
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
         }
     },
 };
--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,25 +1,25 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import config from '../config/config';
+import { v2 as cloudinary } from 'cloudinary';
+import config from '../config/config';
 
-// // Configuración de Cloudinary
-// cloudinary.config({
-//     cloud_name: config.cloudinary.cloud_name,
-//     api_key: config.cloudinary.api_key,
-//     api_secret: config.cloudinary.api_secret,
-//     secure: true
-// });
+// Configuración de Cloudinary
+cloudinary.config({
+    cloud_name: config.cloudinary.cloud_name,
+    api_key: config.cloudinary.api_key,
+    api_secret: config.cloudinary.api_secret,
+    secure: true
+});
 
-// // Función para cargar imágenes
-// export async function uploadImage(filePath: string) {
-//     try {
-//       const result = await cloudinary.uploader.upload(filePath, {
-//         folder: 'movieImage'
-//       });
-//       return result;
-//     } catch (error) {
-//       console.error('Error al subir la imagen a Cloudinary:', error);
-//       throw error;
-//     }
-//   }
+// Función para cargar imágenes
+export async function uploadImage(filePath: string) {
+    try {
+      const result = await cloudinary.uploader.upload(filePath, {
+        folder: 'movieImage'
+      });
+      return result;
+    } catch (error) {
+      console.error('Error al subir la imagen a Cloudinary:', error);
+      throw error;
+    }
+  }
 
 
