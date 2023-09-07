5bfebbc2d95b5c3c33c7fd6f845b043efc0cc201
Horacio Martinez <litstyle17@gmail.com>
2023-09-07 19:21:21 +0200

    feat: Add middleware files for authentication with Auth0
    Two middleware files have been added to enable authentication with Auth0 in the application:
    
    1. authCheck.js: This middleware protects routes that require authentication with Auth0. Requests to these routes must provide a valid access token.
    2. authErrorLogger.js: This middleware logs authentication errors to the console for debugging purposes.

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,15 +7,15 @@ type TConfig = { [key: string]: EnvironmentConfig };
 type EnvironmentConfig = {
     app: AppConfig;
     db: MongoDBConfig;
-    //  auth: AuthConfig
+    auth: AuthConfig
 };
 
 type AppConfig = { PORT: string | number };
 type MongoDBConfig = { URI: string };
 type AuthConfig = {
-    origin: string;
-    audience: string;
-    issuer: string;
+    client_origin: string | undefined;
+    audience: string | undefined;
+    issuer: string | undefined;
 }
 
 if (process.env.NODE_ENV === 'production') dotenv.config({ path: '.env.production' });
@@ -32,11 +32,11 @@ const CONFIG: TConfig = {
         db: {
             URI: process.env.MONGO_DB_URI || ''
         },
-        // auth: {
-        //   origin: getEnvVar(APP_ORIGIN),
-        //   audience: getEnvVar(AUTH_AUDIENCE),
-        //   issuer: getEnvVar(AUTH_ISSUER),
-        // }
+        auth: {
+            client_origin: process.env.APP_ORIGIN,
+            audience: process.env.AUTH0_AUDIENCE,
+            issuer: process.env.AUTH0_ISSUER
+        }
     },
     production: {
         app: {
@@ -45,13 +45,14 @@ const CONFIG: TConfig = {
         db: {
             URI: process.env.MONGO_DB_URI || ''
         },
-        // auth: {
-        //   origin: getEnvVar(APP_ORIGIN),
-        //   audience: getEnvVar(AUTH_AUDIENCE),
-        //   issuer: getEnvVar(AUTH_ISSUER),
-        // }
+        auth: {
+            client_origin: process.env.APP_ORIGIN,
+            audience: process.env.AUTH0_AUDIENCE,
+            issuer: process.env.AUTH0_ISSUER
+        }
     },
 };
+
 
 export const CLOUDINARY_CLOUD_NAME = process.env['CLOUDINARY_NAME']
 export const CLOUDINARY_API_KEY = process.env['CLOUDINARY_API_KEY']
