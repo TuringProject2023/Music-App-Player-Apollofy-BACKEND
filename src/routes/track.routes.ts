2219bbf0ffe55159c12c5ecba5ada2173542b9e8
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-09-12 15:31:22 +0200

    Creates routes files

--- src/routes/track.routes.ts
+++ src/routes/track.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { createTrack } from "../controllers/track.controller";
+import { createTrack } from "../controllers/";
 
 const trackRoutes = Router();
 
-trackRoutes.post("/", createTrack);
+trackRoutes
+    .post("/", createTrack)
+    .get("/track", getTrackById)
+    .get("/track", getAllTracks)
+    .put("/track", updateTrackById)
+    .delete("/track", deleteTrackById)
 
 export default trackRoutes;
